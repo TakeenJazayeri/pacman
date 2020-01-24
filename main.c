19761665390d6a15b7ab45fcb3beb12575cf3c25
Takeen Jazayeri <58880401+TakeenJazayeri@users.noreply.github.com>
2020-01-24 23:00:50 +0330

    building graph based on gameboard

--- main.c
+++ main.c
@@ -1,5 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
 //	C:\Users\takee\Desktop\test.txt
+
+typedef struct {
+	int y;
+	int x;
+} Name;
+
+typedef struct element {
+	Name elName;
+	struct element* f[4];
+} Element;
+
+typedef Element* ElementPtr;
+
+void searchAround (char[][4], ElementPtr, ElementPtr[][4]);
+
+void graphize (char Board[][4], int yPac, int xPac, ElementPtr* startP, ElementPtr links[][4]) {
+	links[yPac][xPac] = malloc(sizeof(Element));
+	links[yPac][xPac]->elName.y = yPac;
+	links[yPac][xPac]->elName.x = xPac;
+	*startP = links[yPac][xPac];
+	searchAround(Board, *startP, links);
+}
+
+void searchAround (char Board[][4], ElementPtr p, ElementPtr links[][4]) {
+	int y = p->elName.y;
+	int x = p->elName.x;
+	// right element
+	if (x == 3 || Board[y][x+1] == '#' || links[y][x+1] != NULL) {
+		p->f[0] = NULL;
+	}
+	else {
+		links[y][x+1] = malloc(sizeof(Element));
+		links[y][x+1]->elName.y = y;
+		links[y][x+1]->elName.x = x+1;
+		p->f[0] = links[y][x+1];
+		searchAround(Board, p->f[0], links);
+	}
+	// down element
+	if (y == 3 || Board[y+1][x] == '#' || links[y+1][x] != NULL) {
+		p->f[1] = NULL;
+	}
+	else {
+		links[y+1][x] = malloc(sizeof(Element));
+		links[y+1][x]->elName.y = y+1;
+		links[y+1][x]->elName.x = x;
+		p->f[1] = links[y+1][x];
+		searchAround(Board, p->f[1], links);
+	}
+	// left element
+	if (x == 0 || Board[y][x-1] == '#' || links[y][x-1] != NULL) {
+		p->f[2] = NULL;
+	}
+	else {
+		links[y][x-1] = malloc(sizeof(Element));
+		links[y][x-1]->elName.y = y;
+		links[y][x-1]->elName.x = x-1;
+		p->f[2] = links[y][x-1];
+		searchAround(Board, p->f[2], links);
+	}
+	// up element
+	if (y == 0 || Board[y-1][x] == '#' || links[y-1][x] != NULL) {
+		p->f[3] = NULL;
+	}
+	else {
+		links[y-1][x] = malloc(sizeof(Element));
+		links[y-1][x]->elName.y = y-1;
+		links[y-1][x]->elName.x = x;
+		p->f[3] = links[y-1][x];
+		searchAround(Board, p->f[3], links);
+	}
+}
 
 // function for representing pacman's movement in game board
 void show (char B[][4]) {
@@ -70,6 +142,10 @@ int main(){
 	show(gameBoard);
 
 	int xPac = 0, yPac = 0, x, y;
+	ElementPtr start;
+	ElementPtr links[4][4] = {NULL};
+	graphize(gameBoard, yPac, xPac, &start, links);
+	
 	//finding foods
 	for (y = 0; y < 4; y++)
 		for (x = 0; x < 4; x++)
